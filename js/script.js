1adcede8e80c5be297bea0ab99ba20edc145b487
Camilo Strobio <67642418+camilostrobio@users.noreply.github.com>
2020-07-07 17:02:46 -0300

    UPDATE CAROUSEL
    Finalização do carousel principal. criado todo o estilo do carousel principal.

--- js/script.js
+++ js/script.js
@@ -7,10 +7,18 @@ $(document).ready(function () {
     })
 
     //carrousel principal
-    $('.owl-carousel').owlCarousel({
+    $('#carousel-principal').owlCarousel({
         items:1,
         lazyLoad:true,
         loop:true,
-        margin:10
+        margin:10,
+        nav: true,
+        navSpeed: 1000,
+        navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+        dots: true,
+        dotsSpeed: 1000,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsiveRefreshRate: 10
     });
 })
