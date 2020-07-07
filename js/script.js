fc76ee56b5231808f65eae2a2e3267a4dad2a57a
Camilo Strobio <67642418+camilostrobio@users.noreply.github.com>
2020-07-07 14:33:48 -0300

    CREATE CAROUSEL
    criação de carrocel

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-    $('#nav-toggle').click(function(e){
+$(document).ready(function () {
+    $('#nav-toggle').click(function (e) {
         e.preventDefault();
         //toggle verifica se a classe existe, se existe ela retira se nao adc.
         $(this).toggleClass('active');
         $('.header-collapse').toggleClass('active');
     })
+
+    //carrousel principal
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        lazyLoad:true,
+        loop:true,
+        margin:10
+    });
 })
