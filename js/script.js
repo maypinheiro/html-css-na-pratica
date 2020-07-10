db331b513c01356a3cfd6c52511eabb7ca125b0d
Camilo Strobio <67642418+camilostrobio@users.noreply.github.com>
2020-07-10 18:33:33 -0300

    CREATE TESTEMUNHOS
    criação da linha de testemunhos.

--- js/script.js
+++ js/script.js
@@ -21,4 +21,29 @@ $(document).ready(function () {
         autoplaySpeed: 1000,
         responsiveRefreshRate: 10
     });
+    //carrousel principal
+    $('#carousel-testemunhos').owlCarousel({
+        items:2,
+        loop:true,
+        margin:40,
+        nav: false,
+        navSpeed: 1000,
+        navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+        dots: true,
+        dotsSpeed: 1000,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsiveRefreshRate: 10,
+        responsive:{
+            0:{
+                items:1
+            },
+            960:{
+                items:2
+            },
+            1280:{
+                nav: true
+            }
+        }
+    });
 })
