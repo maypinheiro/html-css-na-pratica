6378695a44856fc6502a0d0eb3a5f5b61e45a972
Camilo Strobio <67642418+camilostrobio@users.noreply.github.com>
2020-07-14 17:15:29 -0300

    create modal
    criação de modal

--- js/script.js
+++ js/script.js
@@ -6,15 +6,65 @@ $(document).ready(function () {
         $('.header-collapse').toggleClass('active');
     })
 
+    // scroll
+    var nav = $('.header-nav'),
+        navHeight = nav.outerHeight(),
+        sections = $('.section');
+
+    $(window).on('scroll', function () {
+        var sTop = $(this).scrollTop();
+
+        if (sTop > navHeight) {
+            $('.header').addClass('fixed');
+        } else {
+            $('.header').removeClass('fixed');
+        }
+
+        // marcação do scroll
+        if (sTop == 0) {
+            nav.find('a').removeClass('active');
+            nav.find('a[href="#home"]').addClass('active');
+        } else {
+            sections.each(function () {
+                var top = $(this).offset().top - navHeight;
+                if (sTop >= top) {
+                    nav.find('a').removeClass('active');
+                    nav.find('a[href="#' + $(this).attr('id') + '"]').toggleClass('active');
+                }
+            });
+        }
+    });
+
+    // navegação
+    nav.find('a').on('click', function(e){
+        e.preventDefault();
+        $('.header-collapse').removeClass('active');
+        $('#nav-toggle').removeClass('active');
+        var target = $(this).attr('href');
+        if(target == "#home"){
+            $('html, body').animate({scrollTop: 0}, 700);
+        } 
+        else {
+            $('html, body').stop().animate({
+                scrollTop: $(target).offset().top
+            }, 700);
+        }
+    });
+    //back-top
+    $('.back-top').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 700);
+    });
+
     //carrousel principal
     $('#carousel-principal').owlCarousel({
-        items:1,
-        lazyLoad:true,
-        loop:true,
-        margin:10,
+        items: 1,
+        lazyLoad: true,
+        loop: true,
+        margin: 10,
         nav: true,
         navSpeed: 1000,
-        navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+        navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
         dots: true,
         dotsSpeed: 1000,
         autoplay: true,
@@ -23,42 +73,41 @@ $(document).ready(function () {
     });
     //carrousel testemunhos
     $('#carousel-testemunhos').owlCarousel({
-        items:2,
-        loop:true,
-        margin:40,
+        items: 2,
+        loop: true,
+        margin: 40,
         nav: false,
         navSpeed: 1000,
-        navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+        navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
         dots: true,
         dotsSpeed: 1000,
         autoplay: true,
         autoplaySpeed: 1000,
         responsiveRefreshRate: 10,
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-            960:{
-                items:2
+            960: {
+                items: 2
             },
-            1280:{
+            1280: {
                 nav: true
             }
         }
     });
-    
+
     // jobs portifolio
-    $('.portifolio-nav li a').click(function(e){
+    $('.portifolio-nav li a').click(function (e) {
         e.preventDefault();
         $('.portifolio-nav li a').removeClass('active');
         $(this).addClass('active');
 
         $('.portifolio').removeClass('visible');
 
-        if(this.id == "all"){
+        if (this.id == "all") {
             $('.portifolio').addClass('visible');
-        } 
-        else {
+        } else {
             $('.portifolio.' + this.id).addClass('visible');
         }
     });
