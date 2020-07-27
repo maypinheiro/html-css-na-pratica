f97614a7cd1c0b7d20c1abe92fe83ecabcef0522
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-27 11:28:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,12 +14,12 @@ $(document).ready(function () {
         });
     });
 
-    $('#nav-toggle').click(function (e) {
-        e.preventDefault();
-        //toggle verifica se a classe existe, se existe ela retira se nao adc.
-        $(this).toggleClass('active');
-        $('.header-collapse').toggleClass('active');
-    })
+        $('#nav-toggle').click(function (e) {
+            e.preventDefault();
+            //toggle verifica se a classe existe, se existe ela retira se nao adc.
+            $(this).toggleClass('active');
+            $('.header-collapse').toggleClass('active');
+        })
 
     // scroll
     var nav = $('.header-nav'),
